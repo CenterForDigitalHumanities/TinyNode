575b17a55f5bd19541ba318c37cb53f3e879f70c
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-07-08 17:58:26 -0500

    polish

--- error-messenger.js
+++ error-messenger.js
@@ -3,7 +3,7 @@
  * We want to send the same error code and message through.  It is assumed to be RESTful and useful.
  * This will also handle generic (500) app level errors, as well as app level 404 errors.
  *
- * @param rerum_error_res A Fetch API Response object from a fetch() to RERUM that encountered an error.  Explanatory text is in .text().
+ * @param rerum_error_res A Fetch API Response object from a fetch() to RERUM that encountered an error.  Explanatory text is in .text().  In some cases it is a unhandled generic (500) app level Error.
  * @param req The Express Request object from the request into TinyNode
  * @param res The Express Response object to send out of TinyNode
  * @param next The Express next() operator, unused here but required to support the middleware chain.
